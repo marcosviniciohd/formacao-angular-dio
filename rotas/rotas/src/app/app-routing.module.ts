585e41aaff152b0a731397fd78644f42b8dfc0ec
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-01-23 07:54:21 -0300

    feat: criar rotas

--- rotas/rotas/src/app/app-routing.module.ts
+++ rotas/rotas/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TitleComponent, pathMatch: 'full' },
+  { path: 'portifolio', component: CardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
