c0dba6cfbac9c0e51f0ab54f2d9e40eabb9a5814
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-03-09 08:09:48 -0300

    add requisicao para API pokemon

--- entendo-services/src/app/components/card/card.component.ts
+++ entendo-services/src/app/components/card/card.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
 
   name: string = 'CHARIZARD';
   attibutesTypes: string[] = ['FIRE', 'ROCK'];
+
+  constructor(
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemon('picatchu').subscribe({
+      next: (res) => console.log(res),
+      error: (err) => console.log(err)
+    });
+  }
 
 }
